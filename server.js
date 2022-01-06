3f67760c8f433b421b2db0f83a7957109b1b35bc
Yejin Kim <ejjing.official@gmail.com>
2022-01-07 02:21:15 +0900

    Implement server

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const path = require("path");
-
 const app = express();
 
-app.use(express.static("public"))
+app.use("/public", express.static(path.resolve(__dirname, "view", "public")));
+// __filename: file명을 포함한 절대 경로
+// __dirname: file명을 제외한 절대 경로
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname,"view","index.html"));
+});
+
+app.listen(8080, () => console.log("Server running..."));
